acf32af36e475e493c0aac771d9a2cd368e02159
Axel Chardevel <chardevel.a@gmail.com>
2017-08-18 16:50:25 +0200

    merge

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import slides from './reducers/slides';
 
-let store = createStore(slides, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(slides, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
